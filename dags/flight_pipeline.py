2b1d99065061cb225bdda0c0ff1248db4f2c4a55
JagdeepSingh13 <jagdeeps3105@gmail.com>
2026-05-18 02:52:00 +0530

    gold aggr task

--- dags/flight_pipeline.py
+++ dags/flight_pipeline.py
@@ -11,6 +11,7 @@ if str(AIRFLOW_HOME) not in sys.path:
 
 from scripts.bronze_ingest import run_bronze_ingestion
 from scripts.silver_transform import run_silver_transform
+from scripts.gold_aggregate import run_gold_aggregate
 
 default_args = {
     "owner": "airflow",
@@ -36,4 +37,9 @@ with DAG(
         python_callable=run_silver_transform
     )
 
-    bronze >> silver
+    gold = PythonOperator(
+        task_id="gold_aggregate",
+        python_callable=run_gold_aggregate
+    )
+
+    bronze >> silver >> gold
